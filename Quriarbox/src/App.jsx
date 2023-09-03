fd7f1f3ef00d68b53a3e6a80abf46cdb0d37fe88
abdulkader-dev <abdulkader.freelancer@gmail.com>
2023-09-04 05:44:56 +0600

    add tailwind

--- Quriarbox/src/App.jsx
+++ Quriarbox/src/App.jsx
@@ -1,12 +1,18 @@
 
-import './App.css'
+// import './App.css'
 
 function App() {
  
 
   return (
     <>
-     <h1>Hello React</h1> 
+     <h1 className="text-3xl text-center text-amber-600">
+      Hello world!
+    </h1>     
+     <h1 className="text-3xl text-center">
+      Hello world!
+    </h1>     
+         
     </>
   )
 }
