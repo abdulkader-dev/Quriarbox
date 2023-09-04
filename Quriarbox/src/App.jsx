a92469bdc5e130e05b545bb1eb8a8b91646cebd7
abdulkader-dev <abdulkader.freelancer@gmail.com>
2023-09-04 07:17:03 +0600

    Nav

--- Quriarbox/src/App.jsx
+++ Quriarbox/src/App.jsx
@@ -1,19 +1,16 @@
 
-// import './App.css'
+import './App.css'
+
+import Nav from "./components/nav/Nav"
 
 function App() {
  
 
   return (
-    <>
-     <h1 className="text-3xl text-center text-amber-600">
-      Hello world!
-    </h1>     
-     <h1 className="text-3xl text-center">
-      Hello world!
-    </h1>     
+    <div>
+       <Nav></Nav>  
          
-    </>
+    </div>
   )
 }
 
---
+++ Quriarbox/src/components/nav/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <div className='max-w-container bd-red-500'>
+        <ul className='flex gap-x-2.5 justify-end'>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+        </div>
+    
+    </div>
+  )
+}
+
+export default Nav
